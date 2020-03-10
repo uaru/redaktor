6496760fd70e5e57fff1b77c5f4b137e64462168
wojciech.t <uaru.amphiacantoides@gmail.com>
2020-03-10 18:46:43 +0900

    Migrations

---
+++ db/migrate/20200310053234_create_articles_authors.rb
@@ -0,0 +1,11 @@
+class CreateArticlesAuthors < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles_authors do |t|
+      t.bigint :author_id, type: :bigint, null: false, foreign_key: true
+      t.bigint :bookstore_id, type: :bigint, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
+
