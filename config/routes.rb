a3714aadb73718ac1f3f194dea0a56db8826ef74
wojciech.t <uaru.amphiacantoides@gmail.com>
2020-03-10 20:38:19 +0900

    Root route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   resources :issues, only: [:index]
   resources :articles, only: [:index]
   resources :authors, only: [:index]
+
+  root 'articles_admin/admin#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
