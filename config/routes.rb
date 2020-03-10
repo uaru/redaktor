c92f727e188d5ac01fd5f96b723bfa53290a583a
wojciech.t <uaru.amphiacantoides@gmail.com>
2020-03-10 19:05:27 +0900

    Change routes to accept only index for unlogged users

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  resources :magazines, only: [:index]
+  resources :issues, only: [:index]
+  resources :articles, only: [:index]
+  resources :authors, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
