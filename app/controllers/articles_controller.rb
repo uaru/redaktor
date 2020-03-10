bc408fdfab43eae9acdd3cc18ae73de3e583c342
wojciech.t <uaru.amphiacantoides@gmail.com>
2020-03-10 18:42:37 +0900

    Controllers

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,25 @@
+class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show]
+
+  # GET /articles
+  # GET /articles.json
+  def index
+    @articles = Article.all
+  end
+
+  # GET /articles/1
+  # GET /articles/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def article_params
+      params.require(:article).permit(:title, :notes, :tag_list)
+    end
+end
---
+++ app/controllers/issues_controller.rb
@@ -0,0 +1,25 @@
+class IssuesController < ApplicationController
+  before_action :set_issue, only: [:show]
+
+  # GET /issues
+  # GET /issues.json
+  def index
+    @issues = Issue.all
+  end
+
+  # GET /issues/1
+  # GET /issues/1.json
+  def show
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_issue
+      @issue = Issue.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def issue_params
+      params.require(:issue).permit(:year, :number, :month, :issue_date)
+    end
+end
