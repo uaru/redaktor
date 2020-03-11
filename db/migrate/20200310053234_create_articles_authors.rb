dff7292bc60265365b68d574aef2d7bfb1c5be4d
wojciech.t <uaru.amphiacantoides@gmail.com>
2020-03-12 05:50:25 +0900

    Fix error in migration

--- db/migrate/20200310053234_create_articles_authors.rb
+++ db/migrate/20200310053234_create_articles_authors.rb
@@ -2,7 +2,7 @@ class CreateArticlesAuthors < ActiveRecord::Migration[5.2]
   def change
     create_table :articles_authors do |t|
       t.bigint :author_id, type: :bigint, null: false, foreign_key: true
-      t.bigint :bookstore_id, type: :bigint, null: false, foreign_key: true
+      t.bigint :article_id, type: :bigint, null: false, foreign_key: true
 
       t.timestamps
     end
